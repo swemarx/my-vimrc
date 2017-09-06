780a7b67f604680afae2a9a1a042618f925111c6
swemarx <swemarx@aypee.net>
2017-09-06 09:40:56 +0200

    fix

--- install.sh
+++ install.sh
@@ -3,7 +3,14 @@
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin
 
 # Backup old config
-tar cvfz ~/vim-config-$(date '+%Y%m%d%H%M%S').tgz ~/.vimrc ~/.vim
+if [ -d "~/.vim" -o -f "~/.vimrc" ]; then
+	vimbackup="~/vim-config-$(date '+%Y%m%d%H%M%S').tgz"
+	tar cvfz $vimbackup ~/.vimrc ~/.vim >/dev/null 2>&1
+	echo "Old config backed-up as $vimbackup"
+fi
+
+# Copy .vimrc in-place
+cp .vimrc ~/
 
 # Install VimPlug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
